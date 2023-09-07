77a3f784acb3fa2d8267f32771d32b6a8a196b94
coding-to-music <connors.tom@gmail.com>
2023-09-07 15:14:33 -0500

    Build

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
-import Head from 'next/head'
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
-    <Component {...pageProps} />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+      <Analytics />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
